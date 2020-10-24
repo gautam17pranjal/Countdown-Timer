5d65976e838f766d7e159599becf43bcc319e3a5
Subhash Jha <subhashjha35@gmail.com>
2020-10-24 14:37:23 +0200

    Improved javascript codes and code formatting
    There were several changes that were required to use the new ES standards. For example limiting the scope of the variable by using let instead of var. anonymous functions were replaced by lambda operator. To avoid linting errors in future the codes were formatted. A clearInterval() was introduced to stop the timer from going endless as it was not needed after it hits the new year date.

--- count_script.js
+++ count_script.js
@@ -1,30 +1,35 @@
-$(document).ready(function(){
-    var countDownDate = new Date("Jan 1, 2021").getTime();
-    var x = setInterval(function(){
-        var now = new Date().getTime(); 
-        var distance = countDownDate - now; 
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-        if(seconds<10){
-            seconds = "0"+seconds;
-        }   
-        if(distance > 0){
-            $('.expire').hide();
+$(document).ready(() => {
+    let countDownDate = new Date("Jan 1, 2021").getTime();
+
+    let timer = setInterval(() => {
+        let now = new Date().getTime();
+        let distance = countDownDate - now;
+        let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        
+        if (seconds < 10) {
+            seconds = "0" + seconds;
         }
+        
         $('.days').empty();
         $('.hours').empty();
         $('.minutes').empty();
         $('.seconds').empty();
-        $('.days').append(days+"<br>");
-        $('.hours').append(hours+"<br>");
-        $('.minutes').append(minutes+"<br>");
-        $('.seconds').append(seconds+"<br>");    
-        if (distance < 0) {
+        $('.days').append(days + "<br>");
+        $('.hours').append(hours + "<br>");
+        $('.minutes').append(minutes + "<br>");
+        $('.seconds').append(seconds + "<br>");
+
+        if (distance > 0) {
+            $('.expire').hide();
+        } else {
             $('.large_device').hide();
             $('.small_device').hide();
             $('h1').hide();
+            clearInterval(timer);
         }
     }, 1000);
+
 });
